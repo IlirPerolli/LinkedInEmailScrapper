93e8e9d46680081e27af3fdb9c2cda6dbc53b0a9
Ilir Perolli <ilir_perolli@live.com>
2022-01-27 20:29:49 +0100

    bug fixes

--- Scrapper.py
+++ Scrapper.py
@@ -10,27 +10,27 @@ class Scrapper:
     password_string = ""
     link_username = ""
     number_of_posts = 5
-    SCROLL_PAUSE_TIME = 3
+    PAUSE_TIME = 1
     userEmails = []
     newUsers = []
     previousUsers = []
 
     def __init__(self):
+        self.clearEmailsFile()
         self.credentials()
         self.init()
         self.login()
 
-
+    def clearEmailsFile(self):
+        file = open('emails.txt', 'w')
+        file.close()
     def credentials(self):
 
-        print("Shkruani username e Linkedin (email/username?):")
-        self.username_string = str(input())
+        self.username_string = str(input("Shkruani username e Linkedin (email/username?): "))
         print()
-        print("Shkruani passwordin e Linkedin:")
-        self.password_string = str(input())
+        self.password_string =str(input('Password: '))
         print()
-        print("Shkruani usernamin qe deshironi te analizoni:")
-        self.link_username = str(input())
+        self.link_username = str(input("Shkruani usernamin qe deshironi te analizoni: "))
         # print("Shkruani numrin e posteve qe do te analizoni:")
         # self.number_of_posts = int(input())
     def init(self):
@@ -75,9 +75,11 @@ class Scrapper:
         people_you_may_know = soup.find_all('li', attrs={"class":"pv-browsemap-section__member-container"})
 
         for user in people_you_may_know:
+
             nextUser = "https://www.linkedin.com"+user.a['href']+"overlay/contact-info/"
             if ((user.a['href'][4:][:-1] not in (self.newUsers)) and (user.a['href'][4:][:-1] not in (self.previousUsers)) ):
                 self.newUsers.append(user.a['href'][4:][:-1])
+
             else:
                 continue
             self.browser.get(nextUser)
@@ -86,10 +88,13 @@ class Scrapper:
             userEmail = self.getUserEmail(src)
             if ((userEmail != None) and (userEmail not in self.userEmails) ):
                 self.userEmails.append(userEmail)
+                file = open('emails.txt', 'a')
+                file.write(userEmail + '\n')
+                file.close()
 
             print (self.userEmails)
             print (self.newUsers)
-        time.sleep(self.SCROLL_PAUSE_TIME)
+        time.sleep(self.PAUSE_TIME)
 
         for newUser in self.newUsers:
             if (len(self.newUsers) > 0):
